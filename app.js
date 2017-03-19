5f1abfbb108c909f43f6c603faa00aa2a1d6604b
Christian Todd <christiantodd27@icloud.com>
2017-03-18 21:04:58 -0400

    complete challenge.

--- app.js
+++ app.js
@@ -1,45 +1,90 @@
-function getTokens(rawString) {
-  // NB: `.filter(Boolean)` removes any falsy items from an array
-  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+function getTokens(string) {
+  return string.toLowerCase().match(/\b[^\s]+\b/g).sort();
 }
 
-function getWordData(string) {
-  var words = getTokens(string);
-  var wordData = {};
-  var wordMetrics = {};
+function removeReturns(string) {
+  return string.replace(/\r?\n|\r/g, '');
+}
 
-  for (let i = 0; i < words.length; i++) {
-    if (words[i] in wordData) {
-      wordData[words[i]]++;
-    } else {
-      wordData[words[i]] = 1;
+function getDistinctWordCount(tokens) {
+  var distinctWords = [];
+  for (let i = 0; i < tokens.length; i++) {
+    if (distinctWords.indexOf(tokens[i]) === -1) {
+      distinctWords.push(tokens[i]);
     }
   }
-
-  wordMetrics.count = (function (words) {
-    return getTokens(string).length;
-  })();
-
-  wordMetrics.avgWordLength = (function () {
-    wordLengthArr = [];
-    for (let i = 0; i < words.length; i++) {
-      wordLengthArr.push(words[i].length);
-    }
-    return Number((wordLengthArr.reduce((a, b) => a + b, 0) / wordMetrics.count).toFixed(2));
-  })();
-
-  wordMetrics.uniqueWordCount = (function () {
-    var uniqueWordCount = 0;
-    for (var word in wordData) {
-      if (wordData[word] == 1) {
-        uniqueWordCount++;
-      }
-    }
-    return uniqueWordCount;
-  })();
-
-  //console.log(wordMetrics);
-  return wordMetrics;
+  return distinctWords.length;
 }
 
-getWordData('No surprise when youre on his shoulder like every night night fight bright night');
+function getAvgWordLength(tokens) {
+  var totalLength = tokens.join('').length;
+  return (totalLength / tokens.length).toFixed(2)
+}
+
+function writeToPage(string) {
+  var tokens = getTokens(string);
+  var intDistinctWords = getDistinctWordCount(tokens);
+  var intTotalWords = tokens.length;
+  var avgWordLength = getAvgWordLength(tokens);
+
+  var textReport = $('.js-text-report');
+  textReport.find('.js-word-count').text(intTotalWords);
+  textReport.find('.js-unique-word-count').text(intDistinctWords);
+  textReport.find('.js-avg-word-length').text(avgWordLength + ' characters');
+  textReport.removeClass('hidden');
+}
+
+function handleFormSubmit() {
+  $('.js-text-form').submit(function (e) {
+    e.preventDefault();
+    var userText = $(this).find('#user-text').val();
+    writeToPage(removeReturns(userText));
+  });
+}
+
+$(function () {
+  handleFormSubmit();
+});
+
+
+/* My first attempt */
+
+
+// function getWordData(string) {
+//   var words = getTokens(string);
+//   var wordData = {};
+//   var wordMetrics = {};
+
+//   for (let i = 0; i < words.length; i++) {
+//     if (words[i] in wordData) {
+//       wordData[words[i]]++;
+//     } else {
+//       wordData[words[i]] = 1;
+//     }
+//   }
+
+//   wordMetrics.count = (function (words) {
+//     return getTokens(string).length;
+//   })();
+
+//   wordMetrics.avgWordLength = (function () {
+//     wordLengthArr = [];
+//     for (let i = 0; i < words.length; i++) {
+//       wordLengthArr.push(words[i].length);
+//     }
+//     return Number((wordLengthArr.reduce((a, b) => a + b, 0) / wordMetrics.count).toFixed(2));
+//   })();
+
+//   wordMetrics.uniqueWordCount = (function () {
+//     var uniqueWordCount = 0;
+//     for (var word in wordData) {
+//       if (wordData[word] == 1) {
+//         uniqueWordCount++;
+//       }
+//     }
+//     return uniqueWordCount;
+//   })();
+
+//   console.log(wordMetrics);
+//   return wordMetrics;
+// }
